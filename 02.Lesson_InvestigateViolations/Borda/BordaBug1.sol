fc16f13083a0f8e391445bb022fc14ed3a21fc77
Tadeas Kucera <tadeas@certora.com>
2023-01-09 17:02:43 +0100

    Fix BordaBug1

--- 02.Lesson_InvestigateViolations/Borda/BordaBug1.sol
+++ 02.Lesson_InvestigateViolations/Borda/BordaBug1.sol
@@ -89,8 +89,8 @@ contract Borda is IBorda {
 
         _voters[msg.sender].voted = true;
         voteTo(first, 3);
-        voteTo(second, 3);
-        voteTo(third, 3);
+        voteTo(second, 2);
+        voteTo(third, 1);
         
         return true;
     }
